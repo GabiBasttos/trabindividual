aeec1ca188f9cdf1f2bf6969a706ebd46d2f3c86
GabiBasttos <gabrielabastos14@outlook.com>
2024-06-18 17:34:27 -0300

    feat: subindo atualizações da home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -7,17 +7,19 @@ export function Home() {
   return (
 
     <View style={stylehome.container}>
-      <Image style={{ width: 350, height: 350 }} source={Banner} alt="logo" />
+      <Image style={stylehome.logo} source={Banner} alt="logo" />
 
       <Text style={stylehome.title}>CARTEIRA DE TRABALHO DIGITAL</Text>
 
-      <TouchableOpacity style={stylehome.button}>
+      <View style={stylehome.container2}>
+        <TouchableOpacity style={stylehome.button}>
         <Text style={stylehome.textButton}>Entrar com biometria</Text>
-      </TouchableOpacity>
+       </TouchableOpacity>
 
-      <TouchableOpacity style={stylehome.button}>
-        <Text style={stylehome.textButton}>Entrar com gov.br</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={stylehome.button2}>
+        <Text style={stylehome.textButton2}>Entrar com gov.br</Text>
+        </TouchableOpacity>
+      </View>
 
     </View>
   );
--- src/Pages/Home/style.ts
+++ src/Pages/Home/style.ts
@@ -6,28 +6,63 @@ export const stylehome = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff5d8',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-around',
+        
     },
 
+    container2: {
+        backgroundColor: '#fff5d8',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 15,
+        height: 250,
+    },
+
+    logo: {
+        width: 340, 
+        height: 340,
+        marginTop: 180,
+    },
+
+
     title: {
-        fontSize: 17,
-        color: '#001f5c'
+        fontSize: 18,
+        color: "#2f4a7d",
+        fontWeight: 'bold',
+        marginTop: -150,
+             
     },
 
 
     button:{
         backgroundColor: '#0063ff',
-        borderWidth:1,
-        width: '85%',
+        borderWidth: 0.5,
+        borderColor: '#5c9af3',
+        width: 450,
         alignItems: 'center',
         justifyContent: 'center',
-        borderRadius: 10,
+        borderRadius: 20,
+        padding: 12,
+    },
 
+    button2:{
+        borderWidth: 1.5,
+        borderColor: '#5c9af3',
+        width: 450,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        padding: 12,
     },
 
     textButton:{
         fontSize: 15,
         color: "#fff"
+    },
+
+    textButton2:{
+        fontSize: 15,
+        color: "#1b51bd"
     },
 
 
