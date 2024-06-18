19b960f407c05605d83d3f78545c8f5b318b33ad
GabiBasttos <gabrielabastos14@outlook.com>
2024-06-18 15:54:33 -0300

    feat: subindo estilização e a pag home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { stylehome } from './style';
+import React from "react";
+import { Text, TextInput, TouchableOpacity, View, Image } from "react-native";
+import { stylehome } from "./style";
+import Banner from "../../Assets/logo.png";
 
-export function Home(){
+export function Home() {
   return (
-    <View style={stylehome.container}>
-        <Text style={stylehome.title}>CARTEIRA DE TRABALHO DIGITAL</Text>
-    </View>
-  )
-}
 
+    <View style={stylehome.container}>
+      <Image style={{ width: 350, height: 350 }} source={Banner} alt="logo" />
+
+      <Text style={stylehome.title}>CARTEIRA DE TRABALHO DIGITAL</Text>
+
+      <TouchableOpacity style={stylehome.button}>
+        <Text style={stylehome.textButton}>Entrar com biometria</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={stylehome.button}>
+        <Text style={stylehome.textButton}>Entrar com gov.br</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
--- src/Pages/Home/style.ts
+++ src/Pages/Home/style.ts
@@ -3,13 +3,32 @@ import { StyleSheet } from "react-native";
 export const stylehome = StyleSheet.create({
 
     container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff5d8',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 
     title: {
-        fontSize: 17
-    }
-})
+        fontSize: 17,
+        color: '#001f5c'
+    },
+
+
+    button:{
+        backgroundColor: '#0063ff',
+        borderWidth:1,
+        width: '85%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+
+    },
+
+    textButton:{
+        fontSize: 15,
+        color: "#fff"
+    },
+
+
+});
